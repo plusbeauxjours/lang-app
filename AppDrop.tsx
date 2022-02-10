d8cbaba099f073ec328bba5f2b2867ff09ee2dc4
plusbeauxjours <plusbeauxjours@gmail.com>
2022-02-10 22:17:48 +0700

    set: Drop project Animated.sequence & parallel & timing

--- AppDrop.tsx
+++ AppDrop.tsx
@@ -1,5 +1,5 @@
 import React, { useRef } from "react";
-import { Animated, Dimensions, PanResponder, View } from "react-native";
+import { Animated, Easing, Dimensions, PanResponder, View } from "react-native";
 
 import styled from "styled-components/native";
 import Ionicons from "react-native-vector-icons/Ionicons";
@@ -11,13 +11,7 @@ const RED = "#e74c3c";
 
 const Container = styled.View`
   flex: 1;
-  background-color: #34495e;
-`;
-
-const Edge = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
+  background-color: ${BLACK_COLOR};
 `;
 
 const Word = styled.Text<{ color: string }>`
@@ -29,30 +23,46 @@ const Word = styled.Text<{ color: string }>`
 const WordContainer = styled(Animated.createAnimatedComponent(View))`
   width: 100px;
   height: 100px;
+  align-self: center;
   justify-content: center;
   align-items: center;
   background-color: ${GREY};
   border-radius: 50px;
+  margin-vertical: 50px;
 `;
 
 const Center = styled.View`
   flex: 3;
   justify-content: center;
   align-items: center;
+  z-index: 10;
 `;
 
 const IconCard = styled(Animated.createAnimatedComponent(View))`
   background-color: white;
   padding: 10px 20px;
   border-radius: 10px;
+  z-index: 10;
+  position: absolute;
 `;
 
 const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 export default function AppDrop() {
   // Values
+  const opacity = useRef(new Animated.Value(1)).current;
   const scale = useRef(new Animated.Value(1)).current;
   const position = useRef(new Animated.ValueXY({ x: 0, y: 0 })).current;
+  const scaleOne = position.y.interpolate({
+    inputRange: [-300, -80],
+    outputRange: [2, 1],
+    extrapolate: "clamp",
+  });
+  const scaleTwo = position.y.interpolate({
+    inputRange: [80, 300],
+    outputRange: [1, 2],
+    extrapolate: "clamp",
+  });
 
   // Animations
   const onPressIn = Animated.spring(scale, {
@@ -67,19 +77,44 @@ export default function AppDrop() {
     toValue: 0,
     useNativeDriver: true,
   });
+  const onDropScale = Animated.timing(scale, {
+    toValue: 0,
+    duration: 50,
+    easing: Easing.linear,
+    useNativeDriver: true,
+  });
+  const onDropOpacity = Animated.timing(opacity, {
+    toValue: 0,
+    duration: 50,
+    easing: Easing.linear,
+    useNativeDriver: true,
+  });
 
   // Pan Responders
   const panResponder = useRef(
     PanResponder.create({
       onStartShouldSetPanResponder: () => true,
       onPanResponderMove: (_, { dx, dy }) => {
+        console.log(dy);
         position.setValue({ x: dx, y: dy });
       },
       onPanResponderGrant: () => {
         onPressIn.start();
       },
-      onPanResponderRelease: () => {
-        Animated.parallel([onPressOut, goHome]).start();
+      onPanResponderRelease: (_, { dy }) => {
+        if (dy < -250 || dy > 250) {
+          Animated.sequence([
+            Animated.parallel([onDropScale, onDropOpacity]),
+            Animated.timing(position, {
+              toValue: 0,
+              duration: 50,
+              easing: Easing.linear,
+              useNativeDriver: true,
+            }),
+          ]).start();
+        } else {
+          Animated.parallel([onPressOut, goHome]).start();
+        }
       },
     })
   ).current;
@@ -88,20 +123,21 @@ export default function AppDrop() {
 
   return (
     <Container>
-      <WordContainer>
+      <WordContainer style={{ transform: [{ scale: scaleOne }] }}>
         <Word color={GREEN}>알아</Word>
       </WordContainer>
       <Center>
         <IconCard
           {...panResponder.panHandlers}
           style={{
+            opacity,
             transform: [...position.getTranslateTransform(), { scale }],
           }}
         >
           <Ionicons name="beer" color={GREY} size={76} />
         </IconCard>
       </Center>
-      <WordContainer>
+      <WordContainer style={{ transform: [{ scale: scaleTwo }] }}>
         <Word color={RED}>몰라</Word>
       </WordContainer>
     </Container>
